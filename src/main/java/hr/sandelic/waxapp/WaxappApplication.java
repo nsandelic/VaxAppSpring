109bac82761b9240727a7f5f79ae86f9f5f24f72
Nikola <nikola.sandelic@hotmail.com>
2021-09-20 17:23:03 +0200

    Initial commit: Basic structure

---
+++ src/main/java/hr/sandelic/waxapp/WaxappApplication.java
@@ -0,0 +1,13 @@
+package hr.sandelic.waxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaxappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaxappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/sandelic/waxapp/WaxappApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.sandelic.waxapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaxappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
